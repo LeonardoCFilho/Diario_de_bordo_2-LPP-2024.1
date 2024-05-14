67a887d20ec9838c1bc3e2c2d684604f77da7eb0
LeonardoCFilho <131294934+LeonardoCFilho@users.noreply.github.com>
2024-05-14 17:55:30 -0300

    Add files via upload

---
+++ Codigos_e_Exemplos/01.Subprogramas/Slide_12/5.Parametro_nomeado.py
@@ -0,0 +1,6 @@
+def Concatena3Em1 (centena,dezena,unidade):
+    numeroConcatenado = (centena*100)+(dezena*10)+(unidade*1)
+    print(numeroConcatenado)
+
+Concatena3Em1(1,2,3)
+Concatena3Em1(unidade=1,dezena=2,centena=3)
